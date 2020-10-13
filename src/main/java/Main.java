0ec1b486cafeb0f9a3c82d39f348e7c57f63c3bb
Weronika Mirek <werussia16@gmail.com>
2020-10-13 12:57:28 +0200

    reading input from file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,40 @@
 import greedy_salesman.GreedySalesman;
 import greedy_salesman.Path;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashSet;
+import java.util.Scanner;
 import java.util.Set;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Set<Path> paths = new HashSet<>();
-        paths.add(new Path("A", "B", 1));
-        paths.add(new Path("B", "C", 5));
-        paths.add(new Path("A", "C", 4));
-        paths.add(new Path("B", "D", 2));
-        paths.add(new Path("A", "D", 3));
-        paths.add(new Path("D", "C", 6));
+    public static void main(String[] args) throws IOException {
+        BufferedReader file = getInputFile();
 
-        System.out.print(new GreedySalesman(paths, "A").travel());
+        String originCity = file.readLine();
+        Set<Path> paths = getPaths(file);
+
+        System.out.print(new GreedySalesman(paths, originCity).travel());
+    }
+
+    private static BufferedReader getInputFile() throws FileNotFoundException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input file:");
+        String fileName = scanner.next();
+        return new BufferedReader(new FileReader(fileName));
+    }
+
+    private static Set<Path> getPaths(BufferedReader file) throws IOException {
+        Set<Path> paths = new HashSet<>();
+        String line = file.readLine();
+        while (line != null) {
+            String[] path = line.split(",");
+            paths.add(new Path(path[0], path[1], Integer.valueOf(path[2])));
+            line = file.readLine();
+        }
+        return paths;
     }
 }
